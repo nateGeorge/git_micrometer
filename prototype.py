5ed762ab920ed51a1a52c2d125d59d4cb715a865
wordsforthewise <wordsforthewise@gmail.com>
2016-11-01 07:57:22 -0600

    FEAT: sum up additions and print summarpy

--- prototype.py
+++ prototype.py
@@ -5,6 +5,7 @@
 
 from github import Github, Repository
 import os
+import pandas as pd
 
 # First create a Github instance:
 g = Github("nateGeorge", os.environ['GIT_PASS'])
@@ -17,15 +18,36 @@ user = g.get_user(github_user)
 repo = user.get_repo(repo_name)
 commits = list(repo.get_commits())
 
-commit = repo.get_commit(commits[0].sha)
+#commit = repo.get_commit(commits[0].sha)
 
-commit.author.login # get's user name
 
-commit.files # can check if a code file or pdf or whatever
+authors = []
+additions = []
+for commit in commits:
+    print commit
+    if commit.author is not None:
+        author = commit.author.login # get's user name
+        addition = commit.stats.additions # gets number of lines added)
+        print author, addition
+        authors.append(author)
+        additions.append(addition)
+
+commit_df = pd.DataFrame({'author':authors, 'additions':additions})
+
+# in this case we only care about the first few authors
+ourteam = list(commit_df.author.unique()[:4])
+
+commit_df_team = commit_df[commit_df['author'].isin(ourteam)]
+
+total_add = commit_df_team.groupby('author').sum()
+
+print total_add
+
+#commit.files # can check if a code file or pdf or whatever
 
 # if it's code, see how many lines added deleted
 
-commit.stats.additions # gets number of lines added
+
 
 # as a first approximation, let's just add up number of lines added per user
 
