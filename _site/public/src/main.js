487e09d599aae1cf95b6f0c807f8fdf953585697
wordsforthewise <wordsforthewise@gmail.com>
2016-11-01 09:22:09 -0600

    DOCS: add gh-pages starter

---
+++ _site/public/src/main.js
@@ -0,0 +1,7 @@
+import {header} from './components/header/_header.js';
+// import {analytics} from './components/analytics/_analytics.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	header.init();
+	// analytics.init();
+});
